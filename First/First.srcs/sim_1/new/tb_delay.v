c480502b2c73cb75d517fe90f31efe496cd7c369
pingwin <jaloszewski.adrian@gmail.com>
2017-03-15 08:09:09 +0100

    test improvement

--- First/First.srcs/sim_1/new/tb_delay.v
+++ First/First.srcs/sim_1/new/tb_delay.v
@@ -21,27 +21,25 @@
 
 
 module tb_delay(
-        output[1:0] out
+        output[3:0] out_1,
+        output[3:0] out_2
     );
     
     reg clk = 1'b0;
-    reg in = 1'b0;
+    reg[3:0] in = 4'b0;
     
     initial begin
         while(1)
             #1 clk = ~clk;
     end
     
+    reg[3:0] i = 4'b0;
     initial begin
-        #1 in = ~in;
-        #2 in = ~in;
-        #4 in = ~in;
-        #6 in = ~in;
-        #4 in = ~in;
-        #2 in = ~in;
+        for (; i != 4'b1111; i = i + 1)
+            #2 in = in + 1;
         #10 $finish;
     end
     
-    delay_some # (.DELAY(0)) dut_no_delay (.clk(clk), .in(in), .out(out[1]));
-    delay_some # (.DELAY(3)) dut_delay (.clk(clk), .in(in), .out(out[0]));
+    delay_some # (.DELAY(0), .N(4)) dut_no_delay (.clk(clk), .in(in), .out(out_1));
+    delay_some # (.DELAY(3), .N(4)) dut_delay (.clk(clk), .in(in), .out(out_2));
 endmodule
